48260f22be057e38c25a584196eb2fe638d6beaa
KanekiAyush <thakareayush942006@gmail.com>
2025-11-25 11:01:18 +0530

    tested push

--- RetailX/urls.py
+++ RetailX/urls.py
@@ -21,4 +21,5 @@ from RetailX import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
+    path('test/', views.test),
 ]
--- RetailX/views.py
+++ RetailX/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponseRedirect
 
 def index(request):
     return render(request,'index.html')
+
+def test(request):
+    return render(request,'test.html')
