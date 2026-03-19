443761c555bc4812308c6bf9c36105b1a5c12858
Ayush Thakare <thakareayush942006@gmail.com>
2026-03-20 01:44:51 +0530

    Modified by Ayush: Suppliers Database Registered on Admin!!...

--- AccountsDB/admin.py
+++ AccountsDB/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from AccountsDB.models import Admin, Cashier, Manager, ChatRoom, Message
+from AccountsDB.models import Admin, Cashier, Manager, ChatRoom, Message, Supplier
 # Register your models here.
 
 admin.site.register(Admin)
@@ -7,3 +7,4 @@ admin.site.register(Cashier)
 admin.site.register(Manager)
 admin.site.register(ChatRoom)
 admin.site.register(Message)
+admin.site.register(Supplier)
