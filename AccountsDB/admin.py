a3a040e3c5d7ee225ecdcadcc7a9d64fa474de5e
Ayush Thakare <thakareayush942006@gmail.com>
2026-03-20 00:33:41 +0530

    Modified by Ayush: Database Registerd ChatRoom, Messages!...

--- AccountsDB/admin.py
+++ AccountsDB/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from AccountsDB.models import Admin, Cashier, Manager
+from AccountsDB.models import Admin, Cashier, Manager, ChatRoom, Message
 # Register your models here.
 
 admin.site.register(Admin)
 admin.site.register(Cashier)
 admin.site.register(Manager)
+admin.site.register(ChatRoom)
+admin.site.register(Message)
